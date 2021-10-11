7b76c89b3a351bc66410241527ef3910e023f6ea
YenaChoi00 <choiyn33@naver.com>
2021-10-11 15:34:01 +0900

    Implement slide by DB in character choice page

---
+++ getAllInfo.php
@@ -0,0 +1,45 @@
+<?php
+    // 캐릭터 선택 페이지에서 사용
+    // cname (캐릭터 이름) 세션 변수 생성
+
+    include 'connect.php';
+    session_start();
+    
+    $userId = $_SESSION['current_id'];
+    // 해당 id의 정보 선택
+    $sql = "select * from character_info where m_id='$userId'";
+    $result = mysqli_query($con, $sql);
+
+    class Character {
+        public $name;
+        public $ctype;
+        public $health;
+        public $day;
+        public $water;
+        public $air;
+        public $soil;
+
+        public function __construct($name, $ctype, $health, $day, $water, $air, $soil) {  // 생성자도 무조건 정의해줘야 함
+            $this->name = $name;
+            $this->ctype = $ctype;
+            $this->health = $health;
+            $this->day = $day;
+            $this->water = $water;
+            $this->air = $air;
+            $this->soil = $soil;
+        }            
+    }
+
+    $num = 0;
+    $array = array();
+    if($result){
+        while ($row = mysqli_fetch_array($result)) {    // assoc 는 안됨.. 
+            $array[$num] = new Character($row['cname'], $row['ctype'], $row['health'], $row['day'], $row['water'], $row['air'], $row['soil']);
+            $num++;
+        }
+    }
+    echo json_encode($array); 
+
+    mysqli_close($con);
+
+?>
---
+++ js/choice_toon.js
@@ -0,0 +1,97 @@
+var userInfo = [
+    // {name: '테스트', day: 2, health: 80, ctype: 'woman1', air: 0, soil: 0, water: 0}, ....
+]
+
+const toonFile = {
+    man1: 'toon/man1.png',
+    man2: 'toon/man2.png',
+    man3: 'toon/man3.png',
+    man4: 'toon/man4.png',
+    man5: 'toon/man5.png',
+    man6: 'toon/man6.png',
+    woman1: 'toon/woman1.png',
+    woman2: 'toon/woman2.png',
+    woman3: 'toon/woman3.png',
+    woman4: 'toon/woman4.png',
+    woman5: 'toon/woman5.png',
+    woman6: 'toon/woman6.png',
+}
+
+// 1. 캐릭터 정보 불러옴
+// 2. html에 정보 셋팅
+// 3. 결정하기 누르면, php전달하여 session 변수 생성
+
+function infoRequest() {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', '../getAllInfo.php');
+    xhr.send();
+    xhr.onreadystatechange = function(){
+        if(xhr.readyState === 4 && xhr.status === 200){
+            let json = JSON.parse(xhr.responseText);
+            console.log("받은 정보(json): ", json);
+
+            for (let i = 0; i < json.length; i++) {
+                var user = {
+                    name: '', 
+                    day: 0, 
+                    health: 0, 
+                    ctype: '', 
+                    air: 0, 
+                    soil: 0, 
+                    water: 0
+                }
+
+                user['name'] = json[i]['name'];
+                user['day'] = json[i]['day'];
+                user['health'] = json[i]['health'];
+                user['ctype'] = json[i]['ctype'];
+                user['air'] = json[i]['air'];
+                user['soil'] = json[i]['soil'];
+                user['water'] = json[i]['water'];
+
+                userInfo.push(user);
+            };
+
+            console.log('저장된 사용자 정보:', userInfo);
+            makeSlide();
+        }
+    };
+}
+
+infoRequest();
+
+function makeSlide() {
+    console.log("makeSlide");
+    // 1. <div class="carousel-inner">의 자식으로 <div class="carousel-item"> 생성
+    // 2. 1번의 자식으로 <section class="toon-slide"> 생성 -> 정보에 맞게
+
+    for (let i = 0; i < userInfo.length; i++) {
+        
+        // 하단의 순서 알려주는 막대 생성
+        if(i == 0){
+            $('.carousel-indicators').append('<button type="button" data-bs-target="#carouselIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>');
+
+            var div = $('<div class="carousel-item active"></div>');
+
+        } else{
+            $('.carousel-indicators').append('<button type="button" data-bs-target="#carouselIndicators" data-bs-slide-to="'+i+'" aria-label="Slide '+i+'"></button>');
+
+            var div = $('<div class="carousel-item"></div>');
+        };
+
+        var section = $('<section class="toon-slide"></section>');
+
+        var img = $('<img src="img/toon/'+userInfo[i].ctype+'.png" class="d-block" alt="캐릭터 사진">');
+    
+        var caption = $('<div class="caption fs-5"> <p class="name fs-4">'+userInfo[i].name+'</p> <p>'+userInfo[i].day+'일차</p> <p>체력:'+userInfo[i].health+'</p> <p>수질오염도: '+userInfo[i].water+'</p> <p>대기오염도: '+userInfo[i].air+'</p> <p>토양오염도: '+userInfo[i].soil+'</p> </div>');
+    
+        section.append(img);
+        section.append(caption);
+        
+        div.append(section);
+    
+        $('.carousel-inner').prepend(div); // php에서 받아온 배열은 나중에 만들어진 캐릭터 정보부터 있어서 역순으로 저장됨(prepend 사용)
+
+        
+    }    
+}
