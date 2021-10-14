37c2f289ad7110c0caf775779b3cd7359bd8b056
YenaChoi00 <choiyn33@naver.com>
2021-10-14 14:28:44 +0900

    Add close btn

--- js/ending.js
+++ js/ending.js
@@ -54,6 +54,17 @@ function endingSend() {
     };
 }
 
+function done() {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', '../logout.php');
+    xhr.send();
+    xhr.onreadystatechange = function(){
+        if(xhr.readyState === 4 && xhr.status === 200){
+            console.log(xhr.responseText);
+            location.href = "index.html";
+        }
+    };
+}
 
 $(document).ready(function() {
     endingSend();
