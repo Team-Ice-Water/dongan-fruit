c19cd6adb488ca567ccfe43c88f945e922d09efb
YenaChoi00 <choiyn33@naver.com>
2021-10-21 22:51:44 +0900

    Update to echo id

--- checkLogin.php
+++ checkLogin.php
@@ -2,13 +2,11 @@
     session_start();
 
     if(isset($_SESSION['current_id'])){
-        echo "현재 로그인된 사용자 id는".$_SESSION['current_id']."입니다.";
-        ?>
-    <?php
+        echo $_SESSION['current_id'];
     }
-    else if(!isset($_SESSION['current_id'])){
+    /*else if(!isset($_SESSION['current_id'])){
         echo "로그인 후 접근 가능한 페이지 입니다."."<br>";
         echo "<br> <a href='index.html'> 메인으로 돌아가기 </a>";
         exit();
-    }
+    }*/
 ?>
