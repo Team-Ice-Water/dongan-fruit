38a87f231eab8847386d5860d2e62b07e884adb0
YenaChoi00 <choiyn33@naver.com>
2021-11-05 20:48:52 +0900

    Add normal ending case

--- checkEnd.php
+++ checkEnd.php
@@ -17,12 +17,15 @@
         $water = $row['water'];
         $soil = $row['soil'];
         $air = $row['air'];
+        $day = $row['day'];
 
         $arr = array('water' => $water, 'soil' => $soil, 'air' => $air);
         $max_int = max($arr); #배열중 가장 큰 값 추출 
         $max_int_key = array_search($max_int, $arr); #값이 가리키는 키값 추출
 
-        if( ($water+ $soil+ $air) <= 20){
+        if($day >= 40){
+            echo 'normal_ending';
+        } else if( ($water+ $soil+ $air) <= 20){
             echo 'normal_ending';
         } else if(($health) <= 0){
             echo 'health_ending';
