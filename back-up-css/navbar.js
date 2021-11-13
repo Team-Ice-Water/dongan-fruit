b2c351da0b104b876b41b33c1377a062c4f0e71c
YenaChoi00 <choiyn33@naver.com>
2021-11-13 20:04:20 +0900

    Update Nav Bar css

--- back-up-css/navbar.js
+++ back-up-css/navbar.js
@@ -19,7 +19,7 @@ function setNavBar() {
     switch ($('title').text()) {
         case "만든 사람들":
             $('body').prepend(`
-            <nav class="nav justify-end navbar-expand-lg navbar-light" style="position: fixed; z-index: 999; width: 100%;">
+            <nav class="nav justify-end navbar-expand-lg navbar-light">
                 <div class="container-fluid">
                     <div class="collapse navbar-collapse d-grid gap-4 d-md-flex justify-content-md-end" id="navbarNav">
                         <ul class="navbar-nav">
@@ -49,12 +49,12 @@ function setNavBar() {
         case "회원가입":
         case "로그인":
             $('body').prepend(`
-            <nav class="navbar justify-end navbar-expand-lg navbar-light px-3" style="background-color: #85d6ff; width: 100%; position: fixed; z-index: 999;">
+            <nav class="navbar justify-end navbar-expand-lg navbar-light px-3" style="background-color: #85d6ff; width: 100%;">
                 <div class="container-fluid">
                     <a class="navbar-brand d-md-flex justify-content-md-start" href="#">
                         <img src="/img/슬청생.png" alt="logo" width="30%" class="d-inline-block align-text-top">
                     </a>
-                    <div class="d-flex">
+                    <div class="collapse navbar-collapse d-grid gap-4 d-md-flex justify-content-md-end" id="navbarNav">
                         <ul class="navbar-nav">
                             <li class="nav-item">
                                 <a class="nav-link" data-bs-toggle="tooltip" data-bs-placement="bottom" title="시작화면으로" href="index.html">
@@ -68,7 +68,6 @@ function setNavBar() {
                             </li>
                         </ul>
                     </div>
-                    
                 </div>
             </nav>`);
             break;
@@ -76,7 +75,7 @@ function setNavBar() {
         case "캐릭터 선택":
         case "엔딩 모음":
             $('body').prepend(`
-            <nav class="nav justify-end navbar-expand-lg navbar-light" style="position: fixed; z-index: 999; width: 100%;">
+            <nav class="nav justify-end navbar-expand-lg navbar-light">
                 <div class="container-fluid">
                     <div class="collapse navbar-collapse d-grid gap-4 d-md-flex justify-content-md-end" id="navbarNav">
                         <ul class="navbar-nav">
@@ -106,9 +105,9 @@ function setNavBar() {
 
         case "슬기로운 청지기 생활":
             $('body').prepend(`
-            <nav class="nav justify-end navbar-expand-lg navbar-light" style="position: fixed; z-index: 999; width: 100%;">
+            <nav class="nav justify-end navbar-expand-lg navbar-light">
                 <div class="container-fluid">
-                    <div class="d-flex" id="navbarNav">
+                    <div class="collapse navbar-collapse d-grid gap-4 d-md-flex justify-content-md-end" id="navbarNav">
                         <ul class="navbar-nav">
                             <li class="nav-item fs-5 id-txt">
                                 `+ID+`
@@ -142,7 +141,7 @@ function setNavBar() {
         case "브리핑":
         case "아침 브리핑":
             $('body').prepend(`
-            <nav class="nav justify-end navbar-expand-lg navbar-light" style="position: fixed; z-index: 999; width: 100%;">
+            <nav class="nav justify-end navbar-expand-lg navbar-light">
                 <div class="container-fluid">
                     <div class="collapse navbar-collapse d-grid gap-4 d-md-flex justify-content-md-end" id="navbarNav">
                         <ul class="navbar-nav">
@@ -162,9 +161,9 @@ function setNavBar() {
 
         default:
             $('body').prepend(`
-            <nav class="nav justify-end navbar-expand-lg navbar-light" style="position: fixed; z-index: 999; width: 100%;">
+            <nav class="nav justify-end navbar-expand-lg navbar-light">
                 <div class="container-fluid">
-                    <div class="d-flex" id="navbarNav">
+                    <div class="collapse navbar-collapse d-grid gap-4 d-md-flex justify-content-md-end" id="navbarNav">
                         <ul class="navbar-nav">
                             <li class="nav-item fs-5 id-txt">
                                 `+ID+`
